95a93ddaba6efdf4ed029438b807a46427214b4c
Shubham Rananaware <147210613+ShubhamRananaware@users.noreply.github.com>
2025-01-28 12:24:13 +0530

    Create conversion.js

---
+++ conversion.js
@@ -0,0 +1,37 @@
+let age =25
+
+let age1 = " 25SHubham"
+console.log(typeof age);
+console.log(typeof age1);
+
+let valueInAge= Number(age1)
+console.log(typeof valueInAge);
+console.log(valueInAge);
+
+let score1 = null
+let score2 = undefined
+let score3 = true
+ let valueofScoreInNumber1 = Number(score1)
+ console.log(valueofScoreInNumber1);
+ 
+ let valueofScoreInNumber2 = Number(score2)
+ console.log(valueofScoreInNumber2);
+
+ let valueofScoreInNumber3 = Number(score3)
+ console.log(valueofScoreInNumber3);
+
+ /*
+  Datatypes>>> Number
+  "35">>>>>>>>>> 35
+  "25aCB">>>>>NaN
+  null>>>>>0
+  undefined>>>>> NaN
+  true>>>> <<<<1
+  false>>>>> <<<<0
+********
+"">>>false
+"ss">>>true
+
+
+*/
+
