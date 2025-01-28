e4489c728b9caf4038db4a0d28a4ecba6dbda56b
Shubham Rananaware <147210613+ShubhamRananaware@users.noreply.github.com>
2025-01-28 13:16:54 +0530

    Create comparision.js

---
+++ comparision.js
@@ -0,0 +1,21 @@
+console.log("For Null");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// console.log(null = 0); ==>> it will show error invalid left hand side
+
+
+console.log("For Undefined");
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined = 0);
+ 
+
+// major Difference between == and ===
+
+console.log( "2" == 2);// it will convert "2 " in number and then compare
+
+console.log("2" === 2); // it will strictly check the datatype 
