7ecfcddf2372620a13abc5c3ad4591def636fd98
Shubham Rananaware <147210613+ShubhamRananaware@users.noreply.github.com>
2025-01-28 11:39:10 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+let name ="shubham"
+let age = 25
+let isLoggedin = true
+let undefined ;
+let nullll= null
+
+/*
+ *******PRIMITIVE DATATYPES
+String =""
+Number => 2 to the Power 53
+Boolean = t/F
+Null => Standalon Value (Object TYpe)
+undefined 
+Symbol => Unique
+BigInt => For Big Numbers
+
+********Non Primitive (REfrence)
+
+ Object
+*/
+ 
+ console.log(typeof name);
+ console.log(typeof age );
+ console.log(typeof undefined);
+ console.log(typeof nullll);
+ console.log(typeof isLoggedin);
+ 
+ 
