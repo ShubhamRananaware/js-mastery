1f06c85d207b89f62189f7350074e3edbae56f8a
Shubham Rananaware <147210613+ShubhamRananaware@users.noreply.github.com>
2025-01-28 13:00:11 +0530

    Create conversionOperation.js

---
+++ conversionOperation.js
@@ -0,0 +1,20 @@
+let str1 ="Hello "
+ let str2 = "Sr"
+
+ let str = str1+str2
+ console.log(str);
+
+ console.log("1"+2)
+ console.log("1"+2 +2)
+ console.log(1+"2"+1)
+ console.log(2+2+"1"); 
+ 
+//   best practices
+console.log((2+2) * 4 % 2);
+
+let Counter = 100;
+Counter++
+console.log(Counter);
+
+++Counter
+console.log(Counter);
