fa5823d175e0430b73162db1711e143e6d7b8340
Shubham Rananaware <147210613+ShubhamRananaware@users.noreply.github.com>
2025-01-30 17:45:16 +0530

    while dowhile loop

---
+++ Iteration(For)/doDoWhile.js
@@ -0,0 +1,27 @@
+
+// do while, while
+
+// const i = 0  never use constant variable we cannot change it 
+let i = 0
+while (i <= 10){
+    console.log(i);
+    i++;
+    
+}
+
+const myarr =["Rohit", "Virat ","Surya", "Dhoni","Hardik"]
+let i1=0
+while ( i1 <= myarr.length-1) {
+    console.log(`Circkter ${myarr[i1]}`);
+    i1++;
+    
+}
+
+// do while
+
+ let num=10
+do {
+    console.log(`num is ${num}`);
+    num--;
+} //while (num >1);
+ while (num >= 1);
